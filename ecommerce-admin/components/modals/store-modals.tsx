2c593bb1201733e9b56b39783117148018a20b5d
Haigsov <haigsouv@my.yorku.ca>
2023-12-19 21:45:27 -0500

    Created a store-modal using the modal component

--- ecommerce-admin/components/modals/store-modals.tsx
+++ ecommerce-admin/components/modals/store-modals.tsx
@@ -1,15 +1,22 @@
 "use client";
 
+import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "../ui/modal";
 
+//StoreModal component that acts as a wrapper for the Modal component
 export const StoreModal = () => { 
+    const storeModal = useStoreModal();
+
+    //Return the Modal component with the title, description, isOpen, and onClose props
+    return (
     <Modal
-    title="Create store"
-    description="Add a new store to manage products and categories"
-    isOpen={false}
-    onClose={() => {}}
+        title="Create store"
+        description="Add a new store to manage products and categories"
+        isOpen={false}
+        onClose={() => {}}
     >
         Future Create Store Form
     </Modal>
+    );
 
 };
