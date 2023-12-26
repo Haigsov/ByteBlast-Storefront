c1d58755df8856af0362f5688e1f34888b32addd
Haigsov <haigsouv@my.yorku.ca>
2023-12-26 17:13:24 -0500

    Update layout.tsx
    Reorganized and Added more comments

--- ecommerce-admin/app/layout.tsx
+++ ecommerce-admin/app/layout.tsx
@@ -1,9 +1,15 @@
+// Global imports
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+
+// Local imports
 import { ClerkProvider } from '@clerk/nextjs'
+import { ModalProvider } from '@/providers/modal-provider'
 
+// Styles imports
+import './globals.css'
 
+// Purpose: Root layout for the app.
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -19,7 +25,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
