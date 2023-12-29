afa57961d1dfe9d92c0b4c977b09f8a51a4dca01
Haigsov <haigsouv@my.yorku.ca>
2023-12-28 20:58:16 -0500

    Update page.tsx

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,14 +1,27 @@
 "use client";
 
 import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { use, useEffect } from "react";
 
 
+// StoreModal component that acts as a wrapper for the Modal component
 const SetupPage = () => {
-    return (
+  // Check if the store modal is open
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+
+  useEffect(() => {
+    // If the store modal is not open, open it
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return (
       <div className="p-4">
-        <Modal title="Test" description="Test Description" isOpen onClose={() => {}}>
-          Children
-        </Modal>
+        Root Page
       </div>
     );
 }
