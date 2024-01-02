242f251836a8d953310b93065a24bd27d39a86b4
Haigsov <haigsouv@my.yorku.ca>
2024-01-01 21:58:35 -0500

    Update store-modals.tsx
    Added a form component to take input from user.

--- ecommerce-admin/components/modals/store-modals.tsx
+++ ecommerce-admin/components/modals/store-modals.tsx
@@ -1,11 +1,33 @@
 "use client";
 
+// Global imports
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+// Local imports
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
+
+const formSchema = z.object({
+    name: z.string().min(1),
+});
 
 //StoreModal component that acts as a wrapper for the Modal component
 export const StoreModal = () => { 
     const storeModal = useStoreModal();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        },
+    });
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+        // TODO: Create store
+    };
 
     //Return the Modal component with the title, description, isOpen, and onClose props
     return (
