3c7b6bff1c731cf8d1527de95c52e8a1bb25fbd3
Haigsov <haigsouv@my.yorku.ca>
2024-01-18 23:03:36 -0500

    Replaced placeholder with StoreSwitcher component

--- ecommerce-admin/components/navbar.tsx
+++ ecommerce-admin/components/navbar.tsx
@@ -1,15 +1,16 @@
 /* trunk-ignore-all(prettier) */
+// External imports
 import { UserButton } from "@clerk/nextjs";
 
+// Local imports
 import { MainNav } from "@/components/main-nav";
+import StoreSwitcher from "@/components/store-switcher";
 
 const Navbar = () => {
     return (
         <div className="border-b">
             <div className="flex h-16 items-center px-4">
-                <div>
-                    This will be a store switcher
-                </div>
+                <StoreSwitcher />
                 <MainNav className="mx-6"/>
                 <div className="ml-auto flex items-center space-x-4">
                     <UserButton afterSignOutUrl="/" />
