48d0e105563e1f92f0346bd1a09e95bd979d9ed0
Haigsov <haigsouv@my.yorku.ca>
2024-02-16 17:47:39 -0500

    Update image-upload.tsx

--- ecommerce-admin/components/ui/image-upload.tsx
+++ ecommerce-admin/components/ui/image-upload.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import { Trash } from "lucide-react";
+import Image from "next/image";
+
 import { Button } from "@/components/ui/button";
 
 interface ImageUploadProps {
@@ -35,7 +38,17 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
             <div className="mb-4 flex items-center gap-4">
                 {value.map((url) => (
                     <div key={url} className="relative w-[200px] h-[200px] rounded-md overflow-hidden">
-                        <Button></Button>
+                        <div className="z-10 absolute top-2 right-2">
+                            <Button type="button" onClick={() => onRemove(url)} variant="destructive" size={"icon"}>
+                                <Trash className="h-4 w-4" />
+                            </Button>
+                        </div>
+                        <Image
+                            fill
+                            className="object-cover"
+                            alt="Image"
+                            src={url}
+                        />
                     </div>
                 ))}
             </div>
