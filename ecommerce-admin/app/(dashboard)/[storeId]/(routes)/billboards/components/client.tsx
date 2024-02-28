204a99f3a2ae6f4323ddd280072a022e6a1ecbee
Haigsov <haigsouv@my.yorku.ca>
2024-02-27 23:02:32 -0500

    added interface for billboard client

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -3,13 +3,20 @@
 // Global imports
 import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
+import { Billboard } from "@prisma/client";
 
 // Local imports
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 
-export const BillboardClient = () => {
+interface BillboardClientProps {
+    data: Billboard[]
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({
+    data
+}) => {
     const router = useRouter();
     const params = useParams();
     
@@ -17,7 +24,7 @@ export const BillboardClient = () => {
         <>
             <div className="flex items-center justify-between">
                 <Heading
-                    title={"Billboards (0)"} description={"Manage billboards for your store"}
+                    title={`Billboards (${data.length})`} description={"Manage billboards for your store"}
                 />
                 <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
                     <Plus className="mr-2 h-4 w-4" />
