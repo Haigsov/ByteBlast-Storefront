7a3195dd721e44fc6467dde66a908fc33751d5c0
Haigsov <haigsouv@my.yorku.ca>
2024-03-16 20:55:33 -0400

    Added API path routes for APIAlert

--- ecommerce-admin/components/ui/api-list.tsx
+++ ecommerce-admin/components/ui/api-list.tsx
@@ -2,6 +2,7 @@
 
 import { useParams } from "next/navigation";
 import { useOrigin } from "@/hooks/use-origin";
+import { ApiAlert } from "@/components/ui/api-alert";
 
 interface ApiListProps {
     entityName: string;
@@ -16,9 +17,39 @@ export const ApiList: React.FC<ApiListProps> = ({
     const params = useParams();
     const origin = useOrigin();
 
+    const baseUrl = `${origin}/api/${params.storeId}`;
+
     return (
-        <div>
-            ApiList
-        </div>
+        <>
+            <ApiAlert
+                title="GET"
+                variant="public"
+                description={`${baseUrl}/${entityName}`}
+            />
+
+            <ApiAlert
+                title="GET"
+                variant="public"
+                description={`${baseUrl}/${entityName}/${entityIdName}`}
+            />
+
+            <ApiAlert
+                title="POST"
+                variant="admin"
+                description={`${baseUrl}/${entityName}`}
+            />
+
+            <ApiAlert
+                title="PATCH"
+                variant="admin"
+                description={`${baseUrl}/${entityName}/${entityIdName}`}
+            />
+
+            <ApiAlert
+                title="DELETE"
+                variant="admin"
+                description={`${baseUrl}/${entityName}/${entityIdName}`}
+            />
+        </>
     )
 }
