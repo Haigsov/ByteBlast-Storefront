5cd27cade0c0e893e256267f7528f12d565090b3
Haigsov <haigsouv@my.yorku.ca>
2024-03-19 17:37:01 -0400

    Made changes for column component

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/categories/components/columns.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+// Global imports
+import { ColumnDef } from "@tanstack/react-table"
+
+// Local imports
+import { CellAction } from "./cell-action"
+
+export type CategoryColumn = {
+  id: string
+  name: string
+  billboardLabel: string
+  createdAt: string
+}
+
+export const columns: ColumnDef<CategoryColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "billboard",
+    header: "Billboard",
+    cell: ({ row }) => row.original.billboardLabel,k
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original}/>,
+  }
+]
