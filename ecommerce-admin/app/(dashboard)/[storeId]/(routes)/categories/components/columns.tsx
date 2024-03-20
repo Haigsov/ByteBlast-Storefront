d6e03539409eeb178e43bf14be2f95f8369d8c3c
Haigsov <haigsouv@my.yorku.ca>
2024-03-19 23:38:38 -0400

    removed small error

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/categories/components/columns.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/categories/components/columns.tsx
@@ -21,7 +21,7 @@ export const columns: ColumnDef<CategoryColumn>[] = [
   {
     accessorKey: "billboard",
     header: "Billboard",
-    cell: ({ row }) => row.original.billboardLabel,k
+    cell: ({ row }) => row.original.billboardLabel,
   },
   {
     accessorKey: "createdAt",
